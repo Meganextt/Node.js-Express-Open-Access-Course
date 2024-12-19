cf85036ea6e5f28b488b5df3a45ebfc64085d4a3
Meganextt <megan.extt@gmail.com>
2024-12-19 15:13:18 +0700

    Save

--- app.js
+++ app.js
@@ -2,22 +2,24 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan')
+const path = require('path')
 
 const app = express();
 const port = 3000;
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, "/Public/")))
 
 //จัดการรีเควสที่เข้ามาที่เข้ามาใน ถ้าเข้ามาในพาร์ทของ / แล้วจะตอบสนองรีเควสเรสป้อนอย่างไร แล้วให้ใส่คำสั่งลงไปใน{}
-app.get("/",(req,res) =>{
+app.get("/", (req, res) => {
 
     res.send('Hello BorntoDev');
 })
 
 
 //ให้รอฟังที่port และสิ่งที่เขาจะทำใส่ลงไปในปีกกาเหมือนเดิม
-app.listen(port,()=> {
+app.listen(port, () => {
 
-    debug('Listening on port : '+ chalk.green(port));
+    debug('Listening on port : ' + chalk.green(port));
 
 })
